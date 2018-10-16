02e5dabe04f6daa61a0488ede90df07fc218bd96
Thiago Lopes Fazzi <thiago@xtwo.com.br>
2018-10-16 09:09:51 +0200

    clean up credentials

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -6,8 +6,8 @@ export const BASE_URL = 'https://sandbox-api.myparcel.com/v1'
 export const BASE_URL_AUTH = 'https://sandbox-auth.myparcel.com/access-token'
 
 export const CREDENTIALS = {
-  "grant_type": "client_credentials",
-  "client_id": "5eb32787-07db-4898-91e4-68b1b24d6a1a",
-  "client_secret": "iah2Vg1uI6Q3i45Tq7UmjnA2J1Sse329bVRnVOE66ETk73ninmhYRac4RPng4KIy",
+  "grant_type": "",
+  "client_id": "",
+  "client_secret": "",
   "scope": "*"
 }
